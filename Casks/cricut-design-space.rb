db68200809aa5e8ee40d039e43564617b7c9abd6
Parker Wahle <regulad@regulad.xyz>
2026-02-03 16:12:05 -0500

    dummy value for URL do

--- Casks/cricut-design-space.rb
+++ Casks/cricut-design-space.rb
@@ -2,10 +2,10 @@ cask "cricut-design-space" do
   version "9.54.64"
   sha256 "b266d99126d005622eb636ad80387306b706723374a52b801963fce83ff72ef3"
 
-  url "" do
+  url "https://apis.cricut.com/desktopdownload/InstallerFile?fileName=CricutDesignSpace-Install-v#{version}.dmg&operatingSystem=osxnative" do |url|
     require "json"
     require "open-uri"
-    json_data = JSON.parse(URI.open("https://apis.cricut.com/desktopdownload/InstallerFile?fileName=CricutDesignSpace-Install-v#{version}.dmg&operatingSystem=osxnative").read)
+    json_data = JSON.parse(URI.open(url).read)
     json_data["result"]
   end
   name "Cricut Design Space"
