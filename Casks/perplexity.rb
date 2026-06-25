666dfe3e666b80a132f8fad5b7ec8f67a87810b8
Parker Wahle <regulad@regulad.xyz>
2026-06-25 10:33:49 -0400

    Perplexity

---
+++ Casks/perplexity.rb
@@ -0,0 +1,27 @@
+cask "perplexity" do
+  version ":latest"
+  sha256 ":no_check"
+
+  url "https://macos-download.perplexity.ai/Perplexity.dmg"
+  name "Perplexity"
+  desc "Perplexity app for macOS"
+  homepage "https://www.perplexity.ai"
+
+  depends_on macos: "sequoia"
+
+  app "Perplexity.dmg"
+
+  zap trash: [
+       "~/.perplexity-local-browser.sock",
+       "~/Library/Application Scripts/7S8W4W365S.ai.perplexity.macv3.shared",
+       "~/Library/Application Scripts/group.ai.perplexity.app",
+       "~/Library/Application Support/ai.perplexity.macv3*",
+       "~/Library/Caches/ai.perplexity.macv3*",
+       "~/Library/Group Containers/7S8W4W365S.ai.perplexity.macv3.shared",
+       "~/Library/Group Containers/group.ai.perplexity.app",
+       "~/Library/HTTPStorages/ai.perplexity.macv3*",
+       "~/Library/Preferences/ai.perplexity.macv3.perplexityd.plist",
+       "~/Library/Preferences/ai.perplexity.macv3.plist",
+       "~/Library/WebKit/ai.perplexity.macv3",
+     ]
+end
